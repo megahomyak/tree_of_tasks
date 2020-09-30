7f2a3bc09bd6e41305b44539c43fdc57eeb93532
megahomyak <g.megahomyak@gmail.com>
2020-09-30 19:10:02 +0500

    Deleted utils because of the regex lexer (now I don't need to check if the argument is int, regexes will do it for me)

--- utils.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-
-def is_convertible_to_int(some_value: Any) -> bool:
-    try:
-        int(some_value)
-    except (ValueError, TypeError):
-        return False
-    return True
