5e58d4a971fb3851dc094620c7889abbe79457a3
megahomyak <g.megahomyak@gmail.com>
2020-10-03 16:52:50 +0500

    Fixed sqlalchemy session creating

--- db_apis.py
+++ db_apis.py
@@ -9,7 +9,7 @@ import orm_classes
 def get_sqlalchemy_db_session(path_to_db: str) -> sqlalchemy.orm.Session:
     sql_engine = create_engine(path_to_db)
     orm_classes.DeclarativeBase.metadata.create_all(sql_engine)
-    return sqlalchemy.orm.Session(sqlalchemy)
+    return sqlalchemy.orm.Session(sql_engine)
 
 
 class TasksManager:
