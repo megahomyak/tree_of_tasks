63db739d5633cca45474b8a7deaaa03cf9b58684
megahomyak <g.megahomyak@gmail.com>
2020-11-18 23:28:20 +0500

    Added typing for CommandsConstantMetadata.get_data_from_context

--- lexer/constant_metadata_implementations.py
+++ lexer/constant_metadata_implementations.py
@@ -1,10 +1,11 @@
-from typing import Any
+from typing import Tuple, List
 
-from lexer.lexer_classes import BaseConstantMetadata, ConstantContext
+from lexer.lexer_classes import BaseConstantMetadata, ConstantContext, Command
 
 
 class CommandsConstantMetadata(BaseConstantMetadata):
 
     @staticmethod
-    def get_data_from_context(context: ConstantContext) -> Any:
+    def get_data_from_context(
+            context: ConstantContext) -> Tuple["Command", ...]:
         return context.commands
